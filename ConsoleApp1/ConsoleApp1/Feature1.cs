4acb4a5b09b91bfad3d6e6056f3b98a0cd3dbfb8
Sara Mateus <becaspatricia@gmail.com>
2024-08-01 21:28:21 +0100

    Refactured method

--- ConsoleApp1/ConsoleApp1/Feature1.cs
+++ ConsoleApp1/ConsoleApp1/Feature1.cs
@@ -8,12 +8,9 @@ namespace ConsoleApp1
 {
     internal class Feature1
     {
-        public int Add() 
+        public int Add(int a, int b) 
         {
-            int a = 1;
-            int b = 2;
-            int sum = a + b;
-            return sum;
+            return a + b;
         }
     }
 }
